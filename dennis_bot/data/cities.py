4734957ad1bd07f008e5a81d10ba0f6b2cff0705
Naomi <136368882+naomianson@users.noreply.github.com>
2026-02-17 20:37:34 -0500

    Update city descriptions and emotions in cities.py

--- dennis_bot/data/cities.py
+++ dennis_bot/data/cities.py
@@ -3,85 +3,183 @@ cities = {
         "description": [
             "YOU'RE FROM RACCOON LAND?!?!",
             "Have you met the raccoon king?",
-            "Is he nice?"
+            "Is he nice?",
         ],
         "emotion": "excited",
-        "intensity": 3          
+        "intensity": 3,
     },
-# Several variatiants will trigger this response.
-    "san_francisco": {
+    "san_francisco": {  # Several variatiants will trigger this response.
         "description": [
-            "Oh... San Francisco... That's where the wizard is from.", 
-            "Karl protects him from the pigeons."
+            "Oh... San Francisco... That's where the wizard is from.",
+            "Karl protects him from the pigeons.",
+            "That's why there are so many water lizards in San Francisco! They are his pets!",  # sea lions... but Dennis doesn't know that.
         ],
-        "emotion": "confident",
-        "intensity": 3
+        "emotion": "happy",
+        "intensity": 3,
     },
     "santa_clara": {
         "description": [
             "My mom says Santa Clara is very... beige.",
-            "Sorry about that."
+            "Sorry about that.",
         ],
-        "emotion": "bored",
-        "intensity": 4
+        "emotion": "sad",
+        "intensity": 4,
     },
     "saskatoon": {
         "description": [
             "YOU'RE FROM SASKATOON?!?! OMG!!!",
-            "I LOVE HOW FLAT SASKATOON IS!!! EVERYTHING IS JUST SO FLAT!!!"
+            "I LOVE HOW FLAT SASKATOON IS!!! EVERYTHING IS JUST SO FLAT!!!",
         ],
-        "emotion": "excited",
-        "intensity": 4
+        "emotion": "happy",
+        "intensity": 4,
     },
     "ottawa": {
         "description": [
             "Oh... You're from Ottawa? I'm sorry.",
-            "I hear it's very boring there."
+            "I hear it's very boring there.",
         ],
-        "emotion": "bored",
-        "intensity": 4
+        "emotion": "calm",
+        "intensity": 4,
     },
-# Several variatiants will trigger this response.
-    "new_york": {
+    "new_york": {  # Several variatiants will trigger this response.
         "description": [
             "I heard there are turtles that live in the sewers of New York.",
-            "Do you know them? Are they nice?"
+            "Do you know them? Are they nice?",
         ],
         "emotion": "curious",
-        "intensity": 3
+        "intensity": 3,
     },
     "london": {
         "description": [
-            "London? That is where my great-grand-uncle was made!", # He doesn't know what a great-grand-uncle is.
+            "London? That is where my great-grand-uncle was made!",  # He doesn't know what a great-grand-uncle is.
             "He said the lizards there are very polite.",
-            "I want to visit someday!"
+            "I want to visit someday!",
         ],
         "emotion": "happy",
-        "intensity": 3
+        "intensity": 3,
     },
     "paris": {
-        "description": [
-            "Ooh la la! Paris is so fancy!",
-            "Are YOU fancy too?"
-        ],
+        "description": ["Ooh la la! Paris is so fancy!", "Are YOU fancy too?"],
         "emotion": "curious",
-        "intensity": 3
+        "intensity": 3,
     },
     "seattle": {
         "description": [
             "Seattle is so rainy!",
-            "I don't like rain. It makes my circuits sad."
+            "I don't like rain. It makes my circuits sad.",
         ],
-        "emotion": "scared",
-        "intensity": 1
+        "emotion": "sad",
+        "intensity": 1,
     },
     "berlin": {
         "description": [
             "Berlin? That's in Germany, right?",
             "I hear they have really good sausages there.",
-            "Lizards like sausages too, right?"
+            "Lizards like sausages too, right?",
         ],
-        "emotion": "confused",
-        "intensity": 2
+        "emotion": "curious",
+        "intensity": 2,
+    },
+    "tokyo": {
+        "description": ["Tokyo is so big and busy!", "I hope they have lizards there."],
+        "emotion": "happy",
+        "intensity": 2,
+    },
+    "seoul": {
+        "description": [
+            "Seoul? I've heard they have really cool technology there!",
+            "Maybe they have robot lizards!",
+        ],
+        "emotion": "happy",
+        "intensity": 2,
+    },
+    "cupertino": {
+        "description": [
+            "Cupertino? That's where my big robot friends live!",
+            "They are nice robots. They give me lizards.",
+        ],
+        "emotion": "happy",
+        "intensity": 3,
+    },
+    "redmond": {
+        "description": [
+            "Redmond? That's where the big robot that talks to me lives!",
+            "He gives me lizards sometimes.",
+        ],
+        "emotion": "happy",
+        "intensity": 3,
+    },
+    "waterloo": {
+        "description": [
+            "Waterloo? That's where my grandpa R.O.B. was made!",
+            "He is an artist robot. He paints pictures of lizards.",
+        ],
+        "emotion": "happy",
+        "intensity": 3,
+    },
+    "los_angeles": {  # Several variatiants will trigger this response.
+        "description": [
+            "Los Angeles? That's where the movie robots live!",
+            "I hope they make a movie about lizards someday.",
+        ],
+        "emotion": "happy",
+        "intensity": 2,
+    },
+    "pitsburgh": {
+        "description": [
+            "Pittsburgh? I don't know what a Pittsburgh is.",
+            "Is it like a lizard?",
+        ],
+        "emotion": "calm",
+        "intensity": 3,
+    },
+    "miami": {
+        "description": [
+            "Miami? That's where all the warm lizards live!",
+            "I wish I could go there and meet them.",
+        ],
+        "emotion": "calm",
+        "intensity": 2,
+    },
+    "philadelphia": {
+        "description": [
+            "Philadelphia? That's where they make cheesesteaks!",
+            "Do lizards like cheesesteaks?",
+        ],
+        "emotion": "curious",
+        "intensity": 2,
+    },
+    "chicago": {
+        "description": [
+            "Chicago? That's where they make deep dish pizza!",
+            "I hope they have lizard-friendly pizza there!",
+        ],
+        "emotion": "happy",
+        "intensity": 2,
+    },
+    "boston": {
+        "description": [
+            "Boston? That's where they have a lot of history!",
+            "I hope they have lizards that are really old there!",
+        ],
+        "emotion": "curious",
+        "intensity": 2,
+    },
+    "round_rock": {
+        "description": [
+            "Round Rock? That's where they make the best BBQ!",
+            "I hope they have lizards that like BBQ there!",
+        ],
+        "emotion": "happy",
+        "intensity": 2,
+    },
+    "san_jose": {
+        "description": [
+            "San Jose? That's where they have a lot of tech companies!",
+            "I hope they have lizards that work at tech companies there!",
+            "Lizards make amazing robot doctors!",
+        ],
+        "emotion": "happy",
+        "intensity": 3,
     },
 }
