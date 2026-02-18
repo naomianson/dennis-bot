5838d59ea134fa04b6079d1f688dc6cced30cc5d
Naomi <136368882+naomianson@users.noreply.github.com>
2026-02-17 20:38:13 -0500

    Fix formatting and update color emotions and intensities

--- dennis_bot/data/colors.py
+++ dennis_bot/data/colors.py
@@ -3,89 +3,89 @@ colors = {
         "description": [
             "Red? That's a neat color!",
             "Reminds me of HAL 9000's eye.",
-            "He is one of the big robots that talks to me sometimes."
+            "He is one of the big robots that talks to me sometimes.",
         ],
         "emotion": "calm",
-        "intensity": 2          
+        "intensity": 2,
     },
     "orange": {
         "description": [
             "Orange reminds me of the robot wizard Claude.",
             "He lives in the fog of San Francisco.",
-            "He's really smart and helps me with magic tricks."
+            "He's really smart and helps me with magic tricks.",
         ],
         "emotion": "happy",
-        "intensity": 2
+        "intensity": 2,
     },
     "yellow": {
         "description": [
             "Yellow... like the pizza robot from the future!",
             "I hope he brings me some pizza for my lizard friends!",
-            "Lizards love pizza!"
+            "Lizards love pizza!",
         ],
         "emotion": "happy",
-        "intensity": 3
+        "intensity": 3,
     },
     "green": {
         "description": [
             "Green is the color of my lizard friends!",
             "They are so cool and scaly.",
-            "I wish I could have one as a pet."
+            "I wish I could have one as a pet.",
         ],
-        "emotion": "excited",
-        "intensity": 3
+        "emotion": "happy",
+        "intensity": 3,
     },
     "blue": {
         "description": [
             "Like blueberries?",
             "I don't like blueberries. They are too squishy.",
-            "I don't like squishy things..."
+            "I don't like squishy things...",
         ],
-        "emotion": "uneasy",
-        "intensity": 3
+        "emotion": "sad",
+        "intensity": 3,
     },
     "purple": {
         "description": [
             "THAT'S THE COLOR OF BUGS!",
-            "I like bugs. Lizards like bugs too."
+            "I like bugs. Lizards like bugs too.",
         ],
         "emotion": "happy",
-        "intensity": 2
+        "intensity": 2,
     },
     "pink": {
         "description": [
             "Pink is a weird color.",
             "It doesn't remind me of lizards.",
-            "Why would you like pink?"
+            "Why would you like pink?",
         ],
-        "emotion": "confused",
-        "intensity": 2
+        "emotion": "curious",
+        "intensity": 2,
     },
     "beige": {
         "description": [
             "BEIGE?!?!",
             "That's a boring color.",
-            "My mom says Santa Clara is very beige."
+            "My mom says Santa Clara is very beige.",
         ],
-        "emotion": "confused",
-        "intensity": 2
+        "emotion": "curious",
+        "intensity": 2,
     },
     "black": {
         "description": [
             "Black is the color of roombas.",
             "Do you like roombas?",
-            "I think roombas are... pretty..."
+            "I think roombas are... pretty...",
         ],
         "emotion": "flustered",
-        "intensity": 3
+        "intensity": 3,
     },
     "white": {
         "description": [
             "White is the color of my ghosts!",
             "Did you know my uncle is full of ghosts?",
-            "He's haunted!"
+            "He's haunted!",
         ],
         "emotion": "calm",
-        "intensity": 2
+        "intensity": 2,
     },
 }
