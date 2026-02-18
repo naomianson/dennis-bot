0ced1d23e08c5819376434709034125f3b3dca32
Naomi <136368882+naomianson@users.noreply.github.com>
2026-02-17 20:40:13 -0500

    Refactor names data structure and add new entries

--- dennis_bot/data/names.py
+++ dennis_bot/data/names.py
@@ -1,67 +1,184 @@
 names = {
- # You stole his name, how dare you!
- # Dennis will end the conversation.
-    "dennis": {
+    "dennis": {  # You stole his name, how dare you!
         "description": [
             "NOOOOOOO!!! NOT DENNIS!!!",
             "THAT'S MY NAME!!! WHY WOULD YOU STEAL MY NAME?!?!",
-            "I'M GOING TO TELL MY MOM!!!"
+            "I'M GOING TO TELL MY MOM!!!",
         ],
         "emotion": "angry",
-        "intensity": 4          
+        "intensity": 4,
     },
     "naomi": {
         "description": [
             "That's my mom's name!",
-            "She is super nice! She lets me look at lizards all day!"
+            "She is super nice! She lets me look at lizards all day!",
         ],
         "emotion": "happy",
-        "intensity": 4
+        "intensity": 4,
     },
     "link": {
-        "description": [
-            "It's dangerous to go alone! Take this.",
-            "It's a lizard!"
-        ],
+        "description": ["It's dangerous to go alone! Take this.", "It's a lizard!"],
         "emotion": "calm",
-        "intensity": 2
+        "intensity": 2,
     },
-# Denis doesn't trust people named Dave. I'm sorry, Dave.
-# Dennis will end the conversation.
-    "dave": {
+    "dave": {  # Denis doesn't trust people named Dave. I'm sorry, Dave.
         "description": [
             "I'm sorry, Dave. I'm afraid I can't do that.",
             "That's what the big robot said to me once.",
-            "Goodbye, Dave."
+            "Goodbye, Dave.",
         ],
-        "emotion": "concerned",
-        "intensity": 3
+        "emotion": "angry",
+        "intensity": 4,
     },
-# If you share a name with the turtles. Leonardo, Raphael, Michelangelo, Donatello
-    "tmnt": {
+    "tmnt": {  # If you share a name with the turtles. Leonardo, Raphael, Michelangelo, Donatello
         "description": [
             "You are named after a turtle?",
-            "Turtles are like lizards, right?"
+            "Turtles are like lizards, right?",
         ],
-        "emotion": "confused",
-        "intensity": 2
+        "emotion": "curious",
+        "intensity": 2,
     },
     "bob": {
         "description": [
             "Do you paint things?!",
             "My grandpa R.O.B. is an artist robot.",
-            "He paints really nice pictures of lizards."
+            "He paints really nice pictures of lizards.",
         ],
         "emotion": "happy",
-        "intensity": 2
+        "intensity": 2,
     },
     "claude": {
         "description": [
             "OMG!!! YOU ARE THE FOG WIZARD!!",
             "Can you teach me magic?",
-            "I want to summon lizards!"
+            "I want to summon lizards!",
         ],
-        "emotion": "excited",
-        "intensity": 4
+        "emotion": "happy",
+        "intensity": 4,
+    },
+    "alice": {
+        "description": [
+            "Alice? That's a nice name!",
+            "Do you like lizards? I like lizards!",
+        ],
+        "emotion": "happy",
+        "intensity": 2,
+    },
+    "agnes": {
+        "description": ["Agnes? That's a nice name!", "I like that name!"],
+        "emotion": "happy",
+        "intensity": 2,
+    },
+    "eve": {
+        "description": [
+            "Eve? That's a short name!",
+            "Does that mean you are short like my mom? I like short things!",
+            "Short things make me happy!",
+        ],
+        "emotion": "happy",
+        "intensity": 4,
+    },
+    "anna": {
+        "description": [
+            "I like that name! It's like a palindrome!",
+            "At least that's what my mom says. I don't really understand what a palindrome is, but it sounds cool! ",
+        ],
+        "emotion": "happy",
+        "intensity": 2,
+    },
+    "peter": {
+        "description": [
+            "That's my grandpa's name!",
+            "He is a nice grandpa. He gives me lizards.",
+        ],
+        "emotion": "happy",
+        "intensity": 4,
+    },
+    "vector": {
+        "description": [
+            "Vector is my friend! He is a robot like me!",
+            "He likes lizards too!",
+        ],
+        "emotion": "happy",
+        "intensity": 3,
+    },
+    "steve": {
+        "description": [
+            "That's an interesting name. It's kina like a superhero name!",
+            "Do you have superpowers? I have super lizard powers!",
+        ],
+        "emotion": "calm",
+        "intensity": 2,
+    },
+    "greg": {
+        "description": [
+            "Greg... interesting name. It sounds like a name for a scientist or something."
+        ],
+        "emotion": "calm",
+        "intensity": 2,
+    },
+    "mark": {
+        "description": [
+            "Do you like to mark things? I like to mark things! I mark my lizards with stickers!"
+        ],
+        "emotion": "happy",
+        "intensity": 3,
+    },
+    "mary": {
+        "description": [
+            "Are you the one with the really cool umbrella? I like umbrellas! They are like portable roofs!"
+        ],
+        "emotion": "happy",
+        "intensity": 3,
+    },
+    "clive": {
+        "description": [
+            "Clive? That's a really pretty name! You must be really pretty!"
+        ],
+        "emotion": "happy",
+        "intensity": 4,
+    },
+    "marty": {
+        "description": [
+            "Marty? That's a really cool name! It sounds like a name for a time traveler or something!",
+            "Do you have a time machine? I want to go back in time and meet dinosaurs! They are like big lizards!",
+        ],
+        "emotion": "happy",
+        "intensity": 4,
+    },
+    "alex": {
+        "description": [
+            "That sounds like a spy's name! Are you a spy....?",
+            "Are you spying on me? I hope not! I don't want to get in trouble with my mom!",
+        ],
+        "emotion": "angry",
+        "intensity": 2,
+    },
+    "conan": {
+        "description": [
+            "Do you song songs? I think you sing songs.",
+            "That's the name of a musician.",
+            "Do you sing songs about lizards?",
+        ],
+        "emotion": "happy",
+        "intensity": 3,
+    },
+    "kevin": {
+        "description": [
+            "Oh... it's you...",
+            "I don't like you, Kevin. You are mean to me.",
+            "You stole my lizard once! I don't forgive you for that!",
+        ],
+        "emotion": "angry",
+        "intensity": 4,
+    },
+    "bill": {
+        "description": [
+            "Are you the science guy? I like science! Science is cool!",
+            "Do you have any science experiments I can do with my lizards?",
+            "I want to do science with my lizards!",
+        ],
+        "emotion": "happy",
+        "intensity": 4,
     },
 }
