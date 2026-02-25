2308b4d73a270bdb918680892260015b5271eb86
Naomi <136368882+naomianson@users.noreply.github.com>
2026-02-25 05:40:43 -0500

    Add files via upload

---
+++ dennis_bot/main.py
@@ -0,0 +1,10 @@
+from modules.logic import main as dennis_main
+
+if __name__ == "__main__":
+    print("OMG HI! I'M DENNIS! I LOVE LIZARDS!")
+    print("Do YOU love lizards? You better love lizards!")
+    print("Lizards are AWESOME!\n")
+
+    dennis_main()
+
+    print("\nI gotta go now! I have to look at lizards now! BYE!!")
